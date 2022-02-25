caa23fd069d18d6534300d8c31a3a2270c524ce4
danielei9 <dbs99@hotmail.com>
2022-02-25 17:49:28 +0100

    Working Passenger

--- Passenger.py
+++ Passenger.py
@@ -8,12 +8,17 @@
 
 class Passenger(): #Creamos la clase Passenger
     def __init__(self, name, surname,id_card): #Definimos el atributo
-        self.name = name # Definimos que el atributo name,sera el name asig
-        self.surname = surname # Definimos que el atributo surname, sera el surname asig
-        self.id_card = id_card # Definimos que el atributo id_card, sera el id_card asig
+        self.__name = name # Definimos que el atributo name,sera el name asig
+        self.__surname = surname # Definimos que el atributo surname, sera el surname asig
+        self.__id_card = id_card # Definimos que el atributo id_card, sera el id_card asig
 # Definimos el metodo __str__
     def __str__(self):
-        return ("name: " + self.name + " surname: " + self.surname + " id_card: " + self.id_card)
+        return ("name: " + self.__name + " surname: " + self.__surname + " id_card: " + self.__id_card)
 # Definimos el metodo passenger_data
+#Obtains the data of a passenger
+#    Returns:
+#      name: The passenger's name such as 'Jack'
+#      family_name: The passenger's family name such as 'Shephard'
+#      id_card: The passenger's id card such as '85994003S'
     def passenger_data(self):
-        return self
+        return (self.__name, self.__surname, self.__id_card)
